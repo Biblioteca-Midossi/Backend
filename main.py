bbe0a668933a1275c974b2d6a5c00c34115aff08
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2024-01-31 13:58:25 +0100

    Updated test route just cause

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/api/hello', methods=['GET'])
+@app.route('/api/test', methods=['GET'])
 def hello():
-    return jsonify({'message': 'hello'}), 200
+    return jsonify({'message': 'Test route works! Now try the others! 💀'}), 200
 
 
 if __name__ == '__main__':
