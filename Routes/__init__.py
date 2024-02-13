16808f500935f037bd1bf7c7236be479000d2f53
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2024-02-14 00:30:12 +0100

    Update __init__.py

--- Routes/__init__.py
+++ Routes/__init__.py
@@ -1,12 +1,22 @@
-from flask import jsonify
+from flask import jsonify, Flask
 from .Test import Test
 from .Insert import Insert
 
 
-def register_blueprints(app):
+def register_blueprints(app: Flask):
+
+    # Try putting these in a class where you pass `app`
+    # to initialize it and then register as a Blueprint.
+    # Also substitute Blueprints with Flask RESTful
+    # (Check performance comparison before doing)
+    # Also take a look at Quart and Falcon libraries.
     @app.errorhandler(404)
-    def page_not_found():
-        return jsonify({'error': 'Page not found'}), 400
+    def page_not_found(error):
+        return jsonify({'error': f'{error}'}), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({'error': f'{error}'}), 500
 
     app.register_blueprint(Test('Test'))
     app.register_blueprint(Insert('Insert'))
