5f8ce29f3124b563c9b4cd697674f916d3ab5882
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2024-02-21 22:06:04 +0100

    Rewrite using FastAPI

--- Routes/Books.py
+++ Routes/Books.py
@@ -1,6 +1,1 @@
-from flask import jsonify, Blueprint
 
-
-class Insert(Blueprint):
-    def __init__(self, name):
-        super().__init__(name, __name__)
--- Utils/Database/DbHelper.py
+++ Utils/Database/DbHelper.py
@@ -1,16 +1,32 @@
 import logging
+import os.path
 from typing import Optional, Dict
 
 import mysql.connector as mysql
 
 from dotenv import get_key
 
+if os.path.exists('.env'):
+    database = get_key('.env', 'DBNAME')
+    user = get_key('.env', 'USER')
+    password = get_key('.env', 'PASSWORD')
+    host = get_key('.env', 'HOST')
+    port = get_key('.env', 'PORT')
+else:
+    # These are specific to our use case, but can be changed
+    database = 'biblioteca'
+    user = 'root'
+    password = ''
+    host = 'localhost'
+    port = '3306'
+
+
 database_config: Dict[str, Optional[str]] = {
-    "database": get_key('.env', 'DBNAME'),
-    "user": get_key('.env', 'USER'),
-    "password": get_key('.env', 'PASSWORD'),
-    "host": get_key('.env', 'HOST'),
-    "port": get_key('.env', 'PORT'),
+    "database": database,
+    "user": user,
+    "password": password,
+    "host": host,
+    "port": port,
 }
 
 connection_pool = mysql.pooling.MySQLConnectionPool(pool_name = "pool", **database_config)
