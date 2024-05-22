b34b755f306c32ecf7bc30aab2524343af354ff3
BibliotecaMidossi <157805174+BibliotecaMidossi@users.noreply.github.com>
2024-05-22 16:12:21 +0200

    uhhhhhhhhhhhhhhh

---
+++ SQL Scripts/newscripts/istituti.sql
@@ -0,0 +1,4 @@
+INSERT INTO sql1259724_5.istituti (id_istituto, nome_istituto) VALUES (1, 'ITT');
+INSERT INTO sql1259724_5.istituti (id_istituto, nome_istituto) VALUES (2, 'LAC');
+INSERT INTO sql1259724_5.istituti (id_istituto, nome_istituto) VALUES (3, 'LAV');
+INSERT INTO sql1259724_5.istituti (id_istituto, nome_istituto) VALUES (4, 'EXT');
